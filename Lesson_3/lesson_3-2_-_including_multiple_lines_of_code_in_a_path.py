6d879049fb42095f659bdd668fd352f37437b84b
ArchILLtect <140122527+ArchILLtect@users.noreply.github.com>
2025-01-15 13:25:37 -0600

    Complete Lesson 3 Chapter 2

---
+++ Lesson_3/lesson_3-2_-_including_multiple_lines_of_code_in_a_path.py
@@ -0,0 +1,21 @@
+# Part 1:
+age = int(input("Enter your age: "))
+if age >= 18:
+   print ("Congratulations!")
+   print ("You are old enough to vote.")
+else:
+   print ("I'm sorry.")
+   print ("You are not old enough to vote.")
+print("-" * 80)
+
+# Part 2:
+year = int(input("Enter year: "))
+if year == 1:
+   print ("Freshman")
+if year == 2:
+   print ("Sophomore")
+if year == 3:
+   print ("Junior")
+if year == 4:
+   print ("Senior")
+print("-" * 80)
