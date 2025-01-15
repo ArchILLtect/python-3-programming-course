89f5ce46b06b42c52818aaa561aa98d60a920cf9
ArchILLtect <140122527+ArchILLtect@users.noreply.github.com>
2025-01-15 12:23:34 -0600

    Complete Lesson 2 Chapter 1

---
+++ Lesson_2/lesson_2-1_-_escape_sequences.py
@@ -0,0 +1,20 @@
+#Part 1:
+quote = "The women all said, \"Hello\""
+print (quote)
+print("-" * 80)
+
+#Part 2:
+text = "1\t2\n3"
+print (text)
+print("-" * 80)
+
+#Part 3:
+text = "1\t2\n3"
+print (text)
+  
+data = 4
+print(data)
+data = -12.3
+print(data)
+data = "hello there"
+print(data)
