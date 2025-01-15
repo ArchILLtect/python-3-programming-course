f5154d440c63a8ae7da92d79e312152f9af920d5
ArchILLtect <140122527+ArchILLtect@users.noreply.github.com>
2025-01-15 12:22:51 -0600

    Complete Lesson 1

---
+++ Lesson_1/lesson_1_Assignment.py
@@ -0,0 +1,8 @@
+print("Nick Hanson");
+print("Madison College - Non-credit class");
+print("Intro to Python 3 Programming");
+print("Lesson 1 Assignment");
+print("Print your name and 5 extra lines");
+print("End of assignment! :)");
+
+
