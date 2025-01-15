1b88b9ba28326e4d905e9319aa3b563b1e595b2c
ArchILLtect <140122527+ArchILLtect@users.noreply.github.com>
2025-01-15 12:24:08 -0600

    Complete Lesson 2 Assignment

---
+++ Lesson_2/lesson_2_Assignment.py
@@ -0,0 +1,6 @@
+user_fullname = input("Please enter your name: ")
+user_numberOne = int(input("Please enter a number 0-5: "))
+user_numberTwo = int(input("Please enter a number 5-10: "))
+
+print(user_fullname[user_numberOne:user_numberTwo])
+
