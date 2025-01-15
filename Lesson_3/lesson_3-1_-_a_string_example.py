3a657d00c97f0f9cbcd74face3ff7e2710a522b7
ArchILLtect <140122527+ArchILLtect@users.noreply.github.com>
2025-01-15 13:25:18 -0600

    Complete Lesson 3 Chapter 1

---
+++ Lesson_3/lesson_3-1_-_a_string_example.py
@@ -0,0 +1,20 @@
+# Part 1:
+name = "me"
+if name == "me":
+    print ("the same")
+print("-" * 80)
+
+# Part 2:
+letter = "C"
+if letter < "D":
+    print ("less than D")
+
+if letter > "B":
+    print ("greater than B")
+print("-" * 80)
+
+# Part 3:
+letter = "C"
+if letter < "a":
+    print ("less than a")
+print("-" * 80)
