c287e8dcffe9d14ff72ecb45c69226024ec2f33a
ArchILLtect <140122527+ArchILLtect@users.noreply.github.com>
2025-01-16 13:05:50 -0600

    Complete Lesson 3 Chapter 1

---
+++ Lesson_4/lesson_4-1_-_conditional_loops.py
@@ -0,0 +1,17 @@
+# Part 1:
+number = 1
+while number <= 5:
+         print (number)
+         number = number + 1
+print ("goodbye!")
+print("-" * 80)
+
+# Part 2:
+number = 1
+answer = 'y'
+while answer == 'y':
+         print (number)
+         number = number + 1
+         answer = input(
+                "Do you want the next number? ")
+print("-" * 80)
