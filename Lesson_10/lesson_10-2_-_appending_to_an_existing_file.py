16458ed1084e447ff4fe3c106d6c33ba769b5d39
ArchILLtect <140122527+ArchILLtect@users.noreply.github.com>
2025-01-18 09:00:42 -0600

    Complete Lesson 10 Chapter 2

---
+++ Lesson_10/lesson_10-2_-_appending_to_an_existing_file.py
@@ -0,0 +1,7 @@
+out_file = open('C:/Users/nickh/Documents/mydata2.txt', 'a')
+out_file.write('\nHello')
+out_file.close()
+
+out_file = open('C:/Users/nickh/Documents/mydata2.txt', 'a')
+out_file.write('\nHello')
+out_file.close()
